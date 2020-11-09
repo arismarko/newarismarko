eabd983f8d5f05e9c5aa1a97863c3093e037b014
Aris Markogiannakis <aris.markogiannakis@gmail.com>
2020-11-09 20:14:22 +0000

    changed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Aris Markogiannakis Website',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
