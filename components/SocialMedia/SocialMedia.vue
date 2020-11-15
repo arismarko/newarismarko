9c6290055c6a5132a694b3052ef7130ac15d15fd
Aris Markogiannakis <aris.markogiannakis@gmail.com>
2020-11-15 15:39:20 +0000

    improved userability

--- components/SocialMedia/SocialMedia.vue
+++ components/SocialMedia/SocialMedia.vue
@@ -1,9 +1,9 @@
 <template>
 
    <div :class="$style.socialmedia && isBlack? $style.isBlack : $style.isWhite">
-		Follow me &nbsp; <a href="https://twitter.com/arismarko" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-		<a href="https://github.com/arismarko" target="_blank" ><i class="fa fa-github" aria-hidden="true"></i></a>
-		<a href="https://www.linkedin.com/in/aris-markogiannakis-95b2a5/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+		Follow me &nbsp; <a rel="noreferrer" href="https://twitter.com/arismarko" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+		<a  rel="noreferrer" href="https://github.com/arismarko" target="_blank" ><i class="fa fa-github" aria-hidden="true"></i></a>
+		<a rel="noreferrer" href="https://www.linkedin.com/in/aris-markogiannakis-95b2a5/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
 	</div>
 </template>
 
