9926d934a1879137622c5502eccbc4795dd24374
Aris Markogiannakis <aris.markogiannakis@gmail.com>
2020-11-15 16:25:31 +0000

    converted images from jpeg to webp

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -2,7 +2,7 @@
   <section class="hero is-medium is-bold">
     <div  :class="$style.herobody" class="hero-body">
       <div class="container has-text-centered">
-        <img src="/images/aris.png" alt="me" class="heroimg" />
+        <img src="/images/aris.webp" alt="me" class="heroimg" />
         <h1 class="title ">Aris Markogiannakis</h1>
         <h2 class="subtitle is-bold">
           Senior Developer · Community Leader · Visiting Lecturer
