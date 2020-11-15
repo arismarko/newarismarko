1fb6171dcea670fc0d27eaec4c98ed8570af96d2
Aris Markogiannakis <aris.markogiannakis@gmail.com>
2020-11-15 16:15:49 +0000

    improved userability

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type:'text/css', href: '/css/font-awesome.min.css'},
+      { rel: 'stylesheet', type:'text/css', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'},
       { rel: 'stylesheet', type:'text/css', href: '/css/bulma.min.css'},
       { rel: 'stylesheet', type:'text/css', href: '/css/static.css'}
     ],
